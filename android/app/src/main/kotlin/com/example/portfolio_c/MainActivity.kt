249a4c7c0479fcafd5366d3f6a7d6697c14255f6
Chirag <chiragbhardwaj1u@gmail.com>
2023-10-05 20:09:08 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/portfolio_c/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portfolio_c
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PortfolioApp());
+}
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
