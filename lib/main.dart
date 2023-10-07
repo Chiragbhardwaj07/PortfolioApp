31021931bbd81c1d9a75ec5faa52a06c863676c4
Chirag <chiragbhardwaj1u@gmail.com>
2023-10-08 00:56:05 +0530

    added Sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:social_media_buttons/social_media_buttons.dart';
+
+import 'package:iconify_flutter/iconify_flutter.dart';
+// ignore: depend_on_referenced_packages
+import 'package:colorful_iconify_flutter/icons/vscode_icons.dart';
+// ignore: depend_on_referenced_packages
+import 'package:colorful_iconify_flutter/icons/logos.dart';
+import 'package:portfolio_c/navbar.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -22,6 +28,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blueGrey[800],
         elevation: 0.0,
       ),
+      drawer: NavBar(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
@@ -37,11 +44,6 @@ class Home extends StatelessWidget {
                         backgroundImage: AssetImage('assets/profile.jpg'),
                       ))),
             ),
-            // child: CircleAvatar(
-            //   backgroundColor: Colors.amber,
-            //   backgroundImage: AssetImage('assets/profile.jpg'),
-            //   radius: 50.0,
-            // ),
             Divider(
               height: 40.0,
               color: Colors.blueGrey[800],
@@ -112,13 +114,58 @@ class Home extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 30.0,
+              height: 10.0,
             ),
             Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Icon(Icons.mail, color: const Color.fromRGBO(176, 190, 197, 1)),
-                // Icon(Icolor: Colors.blueGrey[200]),
+                Iconify(VscodeIcons.file_type_cpp3),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Iconify(VscodeIcons.file_type_python),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Iconify(Logos.html_5),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Iconify(Logos.css_3),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Iconify(Logos.javascript),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Iconify(Logos.flutter),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Iconify(
+                  VscodeIcons.file_type_dartlang,
+                ),
               ],
+            ),
+            SizedBox(
+              height: 70.0,
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+
+                // ignore: sort_child_properties_last
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Hire Me!',
+                  ),
+                ),
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                    primary: Colors.blueGrey[900]),
+              ),
             )
           ],
         ),
---
+++ lib/navbar.dart
@@ -0,0 +1,185 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Drawer(
+      backgroundColor: Colors.blueGrey[800],
+      child: ListView(padding: EdgeInsets.fromLTRB(10, 20, 5, 10), children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+                radius: 32,
+                backgroundColor: Colors.blueGrey[300],
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/profile.jpg'),
+                )),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Chirag Bhardwaj',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey[100],
+              ),
+            ),
+            Text(
+              'Student',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.blueGrey[500],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Text(
+                  '10',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey[100],
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Projects',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.blueGrey[500],
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  '1296',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey[100],
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'Ratings',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.blueGrey[500],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.book_rounded,
+                  size: 40,
+                  color: Colors.blueGrey[100],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Projects',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey[100]),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.call,
+                  size: 40,
+                  color: Colors.blueGrey[100],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Contact',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey[100]),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 40,
+                  color: Colors.blueGrey[100],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'About',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey[100]),
+                ),
+              ],
+            ),
+            Divider(
+              height: 370,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.dark_mode,
+                  size: 40,
+                  color: Colors.blueGrey[100],
+                ),
+                SizedBox(
+                  width: 200,
+                ),
+                Icon(
+                  Icons.qr_code_2_outlined,
+                  size: 40,
+                  color: Colors.blueGrey[100],
+                ),
+              ],
+            )
+          ],
+        )
+      ]),
+    ));
+  }
+}
